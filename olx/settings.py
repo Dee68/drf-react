72b8999d103e09691b1b79e2e8a04641cb3ca3cb
Dee68 <ddimie283@gmail.com>
2023-09-05 21:33:05 +0100

    Add correction to host name.

--- olx/settings.py
+++ olx/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['drf-react.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['drf-react-efd045f2d250.herokuapp.com', 'drf-react.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
