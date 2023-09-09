2b3a75d93a00e3c96ad069ec8f29b6eba96c6ddd
Dee68 <ddimie283@gmail.com>
2023-09-09 20:47:07 +0100

    Static files added.

--- olx/urls.py
+++ olx/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-#from django.conf.urls.static import static
+from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
 from rest_framework import permissions
