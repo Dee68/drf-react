fcd0e1314eff2d4fe021c2e34384c3d7e8d74526
Dee68 <ddimie283@gmail.com>
2023-09-09 21:04:41 +0100

    Update url path.

--- olx/urls.py
+++ olx/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-#from django.urls import path, include, re_path
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-from django.conf.urls.static import static
+#from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
 from rest_framework import permissions
@@ -50,12 +50,12 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path(
-    #     '',
-    #     schema_view.with_ui('swagger', cache_timeout=0),
-    #     name='schema-swagger-ui'
-    #     ),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path(
+        '',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+        ),
+    #path('', TemplateView.as_view(template_name='index.html')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.urls.authtoken')),
@@ -85,10 +85,10 @@ urlpatterns = [
         UserWishListItems.as_view()
         ),
     path('api/remove-wishlist/', remove_from_list, name='remove-wishlist'),
-    # re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    # re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
+    #static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
 
